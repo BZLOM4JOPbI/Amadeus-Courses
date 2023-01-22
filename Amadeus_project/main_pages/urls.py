34c7e50ba5fdc00ddc3e2acaf018fb67011ce443
fykzi <boevdima779@gmail.com>
2023-01-22 16:08:45 +0300

    create signup for user

--- Amadeus_project/main_pages/urls.py
+++ Amadeus_project/main_pages/urls.py
@@ -1,9 +1,10 @@
 from . import views
 from django.urls import path
 
+
 urlpatterns = [
-    path('index.html', views.index),
-    path('regist.html', views.regist),
+    path('', views.index),
+    path('test_regist.html', views.SignUpUser.as_view()),
     path('ide.html', views.ide),
     path('login.html', views.login),
 ]
