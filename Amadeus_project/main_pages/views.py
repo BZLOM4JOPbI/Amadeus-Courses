e19fc859b551bfdaab5635d7e1d6b800873fc822
fykzi <boevdima779@gmail.com>
2023-01-22 22:47:50 +0300

    our form regist

--- Amadeus_project/main_pages/views.py
+++ Amadeus_project/main_pages/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
-from .forms import CustomUserCreationForm
+from .forms import CustomUserForm
 
 
-class SignUpUser(CreateView):
+# class SignUpUser(CreateView):
 
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('main_pages/index.html')
-    template_name = 'main_pages/test_regist.html'
+#     form_class = CustomUserForm
+#     success_url = reverse_lazy('main_pages/index.html')
+#     template_name = 'main_pages/test_regist.html'
+
+def regist(request):
+    if request.method == 'POST':
+        form = CustomUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            redirect('home_page')
+    form = CustomUserForm
+    context = {
+        'form': form
+    }
+    return render(request, 'main_pages/regist.html', context)
 
 
 def index(request):
