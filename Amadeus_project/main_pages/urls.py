80bf425fcb709c665fe6810123aedd4a83ac4f4e
fykzi <boevdima779@gmail.com>
2023-01-22 15:01:51 +0300

    create urls readdress

--- Amadeus_project/main_pages/urls.py
+++ Amadeus_project/main_pages/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('index.html', views.index),
+    path('regist.html', views.regist),
+    path('ide.html', views.ide),
+    path('login.html', views.login),
 ]
--- Amadeus_project/main_pages/views.py
+++ Amadeus_project/main_pages/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main_pages/index.html')
+
+
+def regist(request):
+    return render(request, 'main_pages/regist.html')
+
+
+def login(request):
+    return render(request, 'main_pages/login.html')
+
+
+def ide(request):
+    return render(request, 'main_pages/ide.html')
+
