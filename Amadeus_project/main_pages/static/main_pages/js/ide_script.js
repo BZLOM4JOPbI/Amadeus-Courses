2a44e44794c555807e7f4947ddfe0dd63702dbe9
BZLOM4JOPbI <prostoyachelovek@gmail.com>
2023-02-01 00:27:05 +0300

    Консоль в IDE

--- Amadeus_project/main_pages/static/main_pages/js/ide_script.js
+++ Amadeus_project/main_pages/static/main_pages/js/ide_script.js
@@ -15,10 +15,23 @@ const getCode = () => {
 let outputBtn = document.querySelector('.resultBtn');
 let resetBtn = document.querySelector('.resetBtn');
 outputBtn.addEventListener('click', (event) => {
-    let output = new Function(getCode())();
-    (output) ? console.log(output) : true
+    let input = getCode();
+    addLogs(input);
 });
 resetBtn.addEventListener('click', (event) => {
     ide.setValue('');
 });
-
+// Консоль
+const consoleLogs = document.querySelector('.consoleLogs');
+const addLogs = (input) => {
+    const log = document.createElement('li');
+    // input = 'console.oldLog = console.log;console.log = function(value){console.oldLog(value);return value;};' + input;
+    input = 'out = ""; console.log = function(val){out = out + " " + val; return out;};' + input
+    try {
+        input = eval(input);
+    } catch (err) {
+        input = err;
+    }
+    log.textContent = `>  ${input}`;
+    consoleLogs.appendChild(log);
+};
