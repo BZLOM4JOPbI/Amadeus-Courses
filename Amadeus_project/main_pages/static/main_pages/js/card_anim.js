02af6e224fb0237772268b39c375f8b6a35f0312
BZLOM4JOPbI <prostoyachelovek@gmail.com>
2023-02-18 00:57:41 +0300

    Минорный рефакторинг

--- Amadeus_project/main_pages/static/main_pages/js/card_anim.js
+++ Amadeus_project/main_pages/static/main_pages/js/card_anim.js
@@ -1,13 +1,8 @@
-const cards = document.querySelectorAll('.card');
-
-
+const cardsArr = document.querySelectorAll('.card');
 const decreaseSize = (event) => {
     event.target.style.animation = 'decreaseScale .3s';
-    console.log('mouseleave');
 }
-
-
-for (let card of cards) {
+for (let card of cardsArr) {
     card.addEventListener('mouseleave', decreaseSize);
     card.addEventListener('mouseenter', (event) => {
         event.target.style.animation = '';
